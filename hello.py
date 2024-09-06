8a2d1d248fb8db5f8af651985b6eb8606c80e4d8
Zachary Fennie <zachary.fennie@duke.edu>
2024-09-06 13:36:49 -0400

    deleted:    hello.py 	new file:   main.py 	new file:   testRun.py 	deleted:    test_hello.py 	new file:   test_main.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-def add(x, y):
-    return x + y
-
-
-result = add(1, 2)
-print(f"The sum of 1 and 2 is {result}")
---
+++ main.py
@@ -0,0 +1,4 @@
+from testRun import add
+
+if __name__ == "__main__":
+    add(2, 1)
